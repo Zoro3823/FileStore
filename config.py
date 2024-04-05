20bbec9d157163c4a63eb99739bb58d053513cb7
Zoro3823 <143424322+Zoro3823@users.noreply.github.com>
2024-04-05 08:02:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1315975369"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "encoderbot"
-MONGO_URI = "mongodb+srv://Venkat3823:Venkat3823@cluster0.ig0oc9y.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Venkat3823:Venkat3823@cluster0.ig0oc9y.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluser0")
 
 #force sub channel id, if you want enable force sub
